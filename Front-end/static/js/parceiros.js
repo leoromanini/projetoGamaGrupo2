b493f3cab05f4cff468642451367ec15bcae5887
LEONARDO HENRIQUE ROMANINI <987308346@itaud.des.ihf>
2020-07-03 13:41:43 -0300

    Adicionado chart js

--- Front-end/static/js/parceiros.js
+++ Front-end/static/js/parceiros.js
@@ -9,5 +9,27 @@ window.addEventListener('load', function () {
     document.getElementById("falha").innerHTML = falha
     document.getElementById("fraude").innerHTML = fraude
     document.getElementById("nome").innerHTML = nome
-    document.getElementById("volume").innerHTML = "Volumetria: "+volume
+    document.getElementById("volume").innerHTML = "Volumetria: " + volume
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'pie',
+
+        // The data for our dataset
+        data: {
+            labels: ['Sucesso', 'Falha', 'Fraude'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: ["#5cb85c","#f0ad4e","#d9534f"],
+                borderColor: 'rgb(0, 0, 0)',
+                data: [sucesso, falha, fraude]
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
 })
+
+
